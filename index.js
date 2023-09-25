8422badd0a8088f0e71e6537ff3a4e0bb96b18f4
JobVillagran <jobvillagran@gmail.com>
2023-09-25 05:22:06 -0600

    mod(Environment)

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// test
+console.log("Hello world!");
+console.log("Miau Miau");
+
+window.alert("I REALLY LOVE PIZZA");
+
+//this is a comment
+
+/**
+ * This
+ * is
+ * a
+ * multiline
+ * comment
+ */

0e67bdfa26f435e18d681d8a740361c9f27c8350
JobVillagran <jobvillagran@gmail.com>
2023-09-25 06:33:11 -0600

    mod(user input: 25:36)

--- index/03 userInput.js
+++ index/03 userInput.js
@@ -1,6 +1,20 @@
-// How to accept user input 1
+/*
+
+<body>
+    <h1>Hello, world!</h1>
+
+    <label id="myLabel">Enter your name</label><br>
+    <input type="text" id="myText"></input><br>
+    <button type="submit" id="myButton">Submit</button>
+
+</body>
+
+*/
+
+// ********* How to accept user input: *********
 
 // EASY WAY with a window prompt
+
 /*
 let username = window.prompt("What's your name?");
 console.log(username);
@@ -14,4 +28,4 @@ document.getElementById("myButton").onclick = function(){ //on click
    username = document.getElementById("myText").value; //Assign the value in the input to element "myText"
    console.log(username);
    document.getElementById("myLabel").innerHTML = "Hello " + username; //Assign the value in the input to element "myLabel"
-}
+};

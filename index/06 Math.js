06eff51c9dc6ddec02622796128863b1e13bad24
JobVillagran <jobvillagran@gmail.com>
2023-09-26 06:16:28 -0600

    mod(Math: 36:51)

---
+++ index/06 Math.js
@@ -0,0 +1,50 @@
+// Round (Redondear) a number
+
+let x = 3.1416;
+x = Math.round(x);
+console.log(x);
+
+// Round down a number
+let y = 5.789;
+y = Math.floor(y);
+console.log(y);
+
+// Round up a number
+let j = 5.789;
+j = Math.ceil(j); //cealing function
+console.log(j);
+
+// Power up a number
+let i = 10;
+i = Math.pow(i, 2); //power(exponent) function
+console.log(i);
+
+// Square root a number
+let k = 9;
+k = Math.sqrt(k); //square root function
+console.log(k);
+
+// Absolute value function
+let m = -3.14;
+m = Math.abs(m); //Distance away from zero
+console.log(m);
+
+// Minimum and Maximum function
+let a = 10;
+let b = 20;
+let c = 30;
+let minimum;
+let maximum;
+
+minimum = Math.max(a, b, c);
+maximum = Math.min(a, b, c);
+
+console.log(minimum);
+console.log(maximum);
+
+// PI function
+let d;
+d = Math.PI;
+console.log(d);
+
+
